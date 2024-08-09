9c0dbe29e72c45ff03abd64c4943ef41d02cd34c
Samuel Bamgbola <samuelbamgbola@gmail.com>
2024-08-09 15:31:38 +0000

    Finalised prompts for crop, fertilizer and disease recommendations.

--- app/recommendation_engine_with_azureOAI.py
+++ app/recommendation_engine_with_azureOAI.py
@@ -1,5 +1,6 @@
 import os
 from openai import AzureOpenAI
+from openai import OpenAI
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -12,20 +13,21 @@ openai.api_base =  os.getenv('ENDPOINT')
 openai.api_type = 'azure' # Necessary for using the OpenAI library with Azure OpenAI
 openai.api_version = '2023-03-15-preview' # Latest / target version of the API
 
-deployment_name = 'qucoon-ml' # SDK calls this "engine", but naming
-                                           # it "deployment_name" for clarity
+# deployment_name = 'qucoon-ml' # SDK calls this "engine", but naming
+#                                            # it "deployment_name" for clarity
 
-client = AzureOpenAI(
-    api_version=openai.api_version,
-    # https://learn.microsoft.com/en-us/azure/cognitive-services/openai/how-to/create-resource?pivots=web-portal#create-a-resource
-    azure_endpoint=openai.api_base,
-    azure_deployment=deployment_name,
-)
+# client = AzureOpenAI(
+#     api_version = openai.api_version,
+#     # https://learn.microsoft.com/en-us/azure/cognitive-services/openai/how-to/create-resource?pivots=web-portal#create-a-resource
+#     azure_endpoint = openai.api_base,
+#     azure_deployment = deployment_name,
+# )
 
-def read_sample():
+client = OpenAI(api_key = os.getenv('OPENAI_API_KEY'))
+
+def read_sample(file_path):
     # Specify the file path
-    # file_path = 'app/sample.txt'
-    file_path = 'sample.txt'
+    # file_path = 'sample.txt'
 
     # Open the file and read its contents into a string
     with open(file_path, 'r') as file:
@@ -44,11 +46,12 @@ def factor_crop_rec(factor, factor_value, factor_normal, crop_name):
             messages = [
                 {"role": "system", "content": "You are a professional Rural Agronomists that specializes in soil management, crop production, and the application of scientific methods to improve farming practices."},
                 {"role": "user", "content": f"""A farmer wants to plant {crop_name} but has a {factor} level of {factor_value} while the normal level is {factor_normal}. 
-                                                He needs to adjust the soil to cover the deficit of {float(factor_value) - float(factor_normal)}.
-                                                Using these value that the farmer has given and the sample format that will be provided below, generate a recommendation for the farmer to cover this deficit.
+                                                He needs to adjust the soil to cover the change of {float(factor_value) - float(factor_normal)}. If there is an increase, recommend ways to reduce but if there is deficiency, recommend ways to increase.
+                                                Using these value that the farmer has given and the sample format that will be provided below, generate a recommendation for the farmer to cover this change.
+                                                If the change is positive, give practical steps on how to increase the level of {factor}. If the change is negative, give practical steps on how to reduce the level of {factor}.
                                                 
                                                 Sample recommendation:
-                                                {read_sample()}"""}
+                                                {read_sample('app/sample_crop.txt')}"""}
             ]
         )
         return response.choices[0].message.content, exception_status
@@ -56,7 +59,7 @@ def factor_crop_rec(factor, factor_value, factor_normal, crop_name):
         exception_status="YES"
         return e,exception_status 
 
-# print(factor_crop_rec("Potassium", 20, 40, "Rice"))
+# print(factor_crop_rec("Nitrogen", 30, 40, "Beans")[0])
 
 # prefix = "app/sample_recommendations"
 # with open(f"{prefix}/Nitrogen_rice_sample.txt", 'w') as file1:
@@ -67,3 +70,58 @@ def factor_crop_rec(factor, factor_value, factor_normal, crop_name):
 
 # with open(f"{prefix}/Potassium_rice_sample.txt", 'w') as file3:
 #     file3.write(factor_crop_rec("Potassium", 20, 40, "Rice"))
+
+def factor_fert_rec(N,P,K,N_level,P_level,K_level,N_normal,P_normal,K_normal,crop_name):
+    exception_status = "NO"
+    try:
+        completion = client.chat.completions.create(
+            temperature=0.3,
+            # engine=deployment_name,
+            # model="gpt-3.5-turbo",
+            model="gpt-4o",
+            messages = [
+                {"role": "system", "content": "You are a professional Rural Agronomists that specializes in soil management, crop production, and the application of scientific methods to improve farming practices."},
+                {"role": "user", "content": f"""A farmer takes samples of his soil and gets Nitrogen: {N}, Phosphorus {P} and Potassium {K}. He wishes to plant {crop_name}.
+                                            
+                                                A sample recommendation format is provided below when Nitrogen level is high, Phosphorus normal and potassium low.
+                                                Sample recommendation:
+                                                {read_sample('app/sample_fert.txt')}
+                                                
+                                                For this particular farmer, the Nitrogen Level of the soil is {N_level},
+                                                the current Phosphorus Level of the soil is {P_level} when compared to a normal value of {P_normal},
+                                                Potassium Level of the soil is {K_level}.
+                                                
+                                                For this soil profile, using the sample format only, provide a recommendation for the soil to effect {N_level} level Nitrogen, {P_level} level Phosphorus and {K_level} level Potassium but nothing more."""}
+                ]
+
+            )
+        return completion.choices[0].message.content, exception_status
+    except Exception as e:
+        exception_status="YES"
+        return e, exception_status
+
+def factor_disease_rec(crop_name,disease_name):
+    exception_status = "NO"
+    try:
+        completion = client.chat.completions.create(
+            temperature=0.1,
+            # engine=deployment_name,
+            # model="gpt-3.5-turbo",
+            model="gpt-4o",
+            messages = [
+                {"role": "system", "content": "You are a recommender for rural farmers. only use block of text in 100 words strictly and make responses dynamic, The response should be in 2 paragraphs, first paragraph should speak to Causes of the disease and the second paragraph should speak to remedies, you should add kenyan local remedies, if you add Kenyan traditional name, describe it briefly)"},
+                {"role": "user", "content": f"""I have been provided with a crop name and disease it is suffering from. In two sections (Cause, Remedies) with a maximum of two bullet points, generate a recommendation for a case of {crop_name} suffering {disease_name}.
+                                                
+                                                Below is a sample recommendation for tomato suffering from Tomato Late Blight:
+                                                {read_sample('app/sample_disease.txt')}"""},
+            ]
+
+        )
+        return completion.choices[0].message.content, exception_status
+    except Exception as e:
+        exception_status="YES"
+        return e, exception_status
+    
+# print(factor_fert_rec(82,60,50,"Normal","High","Normal",80,42,50,"Rice")[0])
+
+print(factor_disease_rec("Apple","Black rot")[0])
